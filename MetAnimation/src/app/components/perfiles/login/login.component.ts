9255edb02dbd31d7897afa4c85123492ddf42bff
Gian <gian-5634@hotmail.com>
2022-11-29 11:39:10 -0500

    ...

--- MetAnimation/src/app/components/perfiles/login/login.component.ts
+++ MetAnimation/src/app/components/perfiles/login/login.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  InProgress(){
+    alert('En proceso')
+  }
   constructor() { }
 
   ngOnInit(): void {
